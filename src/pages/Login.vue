4fd8c11986ad1d04b68472f6ab22f4a84a025fe4
1612657466daxiong <1612657466@qq.com>
2018-11-27 14:38:14 +0800

    first commit 项目第一次提交

---
+++ src/pages/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="loginBackground" class="background">
+    <div style=" padding-top:20%;">
+
+      <p class="slogan"></p>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    mounted(){
+      this.getLoginHeight();//获取设备高度设置界面高度
+    },
+    methods:{
+      getLoginHeight(){
+        this.$refs.loginBackground.style.height = document.documentElement.clientHeight+"px"
+      }
+    }
+  }
+</script>
+<style>
+  .background{
+    background: url("../assets/login/bg_denglu.png") no-repeat;
+    background-size: 100% 100%;
+    text-align: center;
+  }
+</style>
