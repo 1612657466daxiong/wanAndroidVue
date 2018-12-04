173fae361e22501cc5fa4c4c62d6b4ea39182610
1612657466daxiong <1612657466@qq.com>
2018-12-04 14:24:23 +0800

    首页轮播问题解决，实现轮播文字跟随

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="swiper-box" ref="swiperBox">
+  <div class="swiper-box" ref="swiperBox"  >
     <swiper class="swiper_container" :options="swiperOption" ref="mySwiper"  >
         <swiper-slide v-for="(item,index) in listImage" :key="index" >
           <div>
             <img class="swiper-image" :src="item.url" :alt="item.name" >
-
           </div>
         </swiper-slide>
-      <div class="swiper-pagination swiper-pagination-fraction" slot="pagination" ref="swiperFraction">
-
-    </div>
+      <div class="swiper-pagination swiper-pagination-fraction" slot="pagination" ref="swiperFraction"></div>
     </swiper>
-    <span id="span_ceshi" style="color: #0BAD47;font-size: 14px" v-text="this.listImage[this.activeIndexSwiper].name" ></span>
+    <div ref="bottom_text" id="span_ceshi" class="float_span"  ></div>
   </div>
+
 </template>
 
 <script>
@@ -22,10 +20,9 @@
       swiper,
       swiperSlide
     },
+
     data() {
       return {
-
-        activeIndexSwiper:0,
         listImage:[
           {name:"Android1", url:require('../assets/login/Androidbackground.jpg')},
           {name:"Android2",url:require('../assets/login/androidroot.jpg')},
@@ -49,27 +46,16 @@
             type:'fraction'
           },
           on:{
-
-            // slideChangeTransitionEnd:()=>this.showText()
-            // slideChangeTransitionEnd:()=>this.getIndexToCallBack(this.swiper,this.showText)
-            // slideChangeTransitionEnd:()=>{
-            //   console.log(this)
-            //   // let swiper = this.$refs.mySwiper.swiper;
-            //   let i =  this.$refs.mySwiper.swiper.activeIndex;
-            //   this.activeIndexSwiper = i;
-            //   // let flag =this.listImage[i];
-            //   // console.error(flag.name)
-            // },
             slideChangeTransitionEnd: function(){
-              console.error("翻页")
-              // let i = this.$refs.mySwiper.swiper.activeIndex;
-              // console.error(i)
-              console.log(this,this.activeIndex)
+
               let  i =this.activeIndex;
-              console.error(i)
-             
-              // that.activeIndexSwiper = i;
-              console.error(this.activeIndexSwiper)
+              if(i>window._app.listImage.length){
+                i = i%window._app.listImage.length -1
+              }else {
+                i= i-1
+              }
+              var  r =document.getElementById("span_ceshi");
+              r.innerText=window._app.listImage[i].name
             },
 
           }
@@ -86,23 +72,19 @@
 
       this.getLoginHeight();
       console.log('this is current swiper instance object', this.swiper)
-      this.swiper.slideTo(4,1000,false)
+      this.swiper.slideTo(1,1000,false)
 
+    },
+    created(){
+      window._app = this;
     },
     methods:{
       getLoginHeight(){
         this.$refs.swiperFraction.style.marginLeft = (document.documentElement.clientWidth-this.$refs.swiperFraction.style.width-100)+"px"
       },
-      showText(index){
-
-        var  r =document.getElementById("span_ceshi");
-        r.innerText=this.listImage[index].name
-      },
-      getIndexToCallBack(index,callback){
-        console.error(index)
-        callback(index.activeIndex)
+      setBottomText(){
+        // this.$refs.bottom_text.
       }
-
   },
 
   }
@@ -116,7 +98,7 @@
   }
   .swiper-pagination{
     width: 100px;
-    background: #770000;
+    z-index: 9999;
   }
   .swiper_pagination_content{
     height: 50px;
@@ -125,6 +107,20 @@
 
   }
   .swiper-box{
+    clear: both;
     height: 200px;
+    position: relative;
+  }
+  .float_span{
+    width: 100%;
+    height: 30px;
+    background: rgba(18, 0, 0, 0.2);
+    font-size: 14px;
+    color: white;
+    position: absolute;
+    padding-left: 20px;
+    margin-top: -30px;
+    z-index: 999;
+    vertical-align: middle;
   }
 </style>
